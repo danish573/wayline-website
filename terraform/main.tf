825a7273a3fda0225064c666c11cf4fccf3ba844
danish573 <dkhan573@gmail.com>
2025-10-22 12:20:13 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,7 +63,21 @@ resource "aws_security_group" "wayline_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  # Prometheus
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+# Grafana
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   # Outbound traffic (allow all)
   egress {
     from_port   = 0
