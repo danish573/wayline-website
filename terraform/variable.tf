d93c71a18eb9e87133d3a60cdef2785965125e55
danish573 <dkhan573@gmail.com>
2025-10-23 17:42:25 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,19 +1,19 @@
-variable "aws_region" {
-  description = "AWS Region to setup Server"
-  type        = string
-  default     = "ap-south-1"
-}
+  variable "aws_region" {
+    description = "AWS Region to setup Server"
+    type        = string
+    default     = "ap-south-1"
+  }
 
 variable "ami_id" {
   description = "Ubuntu Image ID"
   type        = string
-  default     = "ami-02d26659fd82cf299" # Ubuntu 22.04 LTS (us-east-1)
+  default     = "ami-07f07a6e1060cd2a8" # Ubuntu 22.04 LTS (us-east-1)
 }
 
 variable "instance_type" {
   description = "Define EC-2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 variable "key_name" {
